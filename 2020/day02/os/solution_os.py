da2bebb5737c3b8f71a6a44d9e8c266d9753c329
Olivier Soucy <osoucy@zoho.com>
2020-12-04 11:40:54 -0500

    Improve my solution

--- 2020/day02/os/solution_os.py
+++ 2020/day02/os/solution_os.py
@@ -1,4 +1,4 @@
-#!usr/bin/env python3
+#!/usr/bin/env python3
 import collections
 import re
 import sys
@@ -6,7 +6,7 @@ import sys
 PasswordValidation = collections.namedtuple("PasswordValidation", ["min", "max", "letter", "password"])
 validations = []
 
-regex = re.compile("(?P<min>\d+)-(?P<max>\d+) (?P<letter>\w): (?P<password>\w+)")
+regex = re.compile(r"(?P<min>\d+)-(?P<max>\d+) (?P<letter>\w): (?P<password>\w+)")
 
 with open(sys.argv[1], 'r') as input_file:
     for line in input_file:
@@ -16,15 +16,15 @@ with open(sys.argv[1], 'r') as input_file:
 
 valid_passwords_part1 = []
 valid_passwords_part2 = []
-for validation in validations:
-    count_letter = validation.password.count(validation.letter)
-    print(validation.min, validation.max, validation.letter, validation.password, count_letter)
-    if count_letter >= validation.min and count_letter <= validation.max:
+for v in validations:
+    count_letter = v.password.count(v.letter)
+    print(v.min, v.max, v.letter, v.password, count_letter)
+    if count_letter >= v.min and count_letter <= v.max:
         print("password is valid for part1")
-        valid_passwords_part1.append(validation.password)
-    if (validation.password[validation.min-1] == validation.letter) != (validation.password[validation.max-1] == validation.letter):
+        valid_passwords_part1.append(v.password)
+    if (v.password[v.min-1] == v.letter) != (v.password[v.max-1] == v.letter):
         print("password is valid for part2")
-        valid_passwords_part2.append(validation.password)
+        valid_passwords_part2.append(v.password)
 
 print("Number of valid passwords for part 1: ", len(valid_passwords_part1))
 print("Number of valid passwords for part 2: ", len(valid_passwords_part2))
