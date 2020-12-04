8c8cd46a851f1b903a19d8923a7de31aa9308055
Kévin Lévesque <klevesque@innovmetric.com>
2020-12-04 10:29:41 -0500

    Solution Kev jour 1

---
+++ 2020/kev/day01/solution_kl.py
@@ -0,0 +1,18 @@
+import functools
+import itertools
+import pathlib
+
+
+def product_of_sum_that_equals(s, numbers, n):
+    for a in itertools.combinations(numbers, n):
+        if sum(a) == s:
+            print(functools.reduce(lambda x, y: x*y, a))
+            break
+
+
+if __name__ == '__main__':
+    input_file = pathlib.Path('input_kev.txt')
+    with input_file.open() as f:
+        numbers = [int(n) for n in f]
+    product_of_sum_that_equals(2020, numbers, 2)
+    product_of_sum_that_equals(2020, numbers, 3)
