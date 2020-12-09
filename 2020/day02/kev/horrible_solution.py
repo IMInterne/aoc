97366ed6df2b62cc4351418003a7bee6b38e5982
Kévin Lévesque <klevesque@innovmetric.com>
2020-12-08 21:56:34 -0500

    improves the horrible solution

--- 2020/day02/kev/horrible_solution.py
+++ 2020/day02/kev/horrible_solution.py
@@ -1,13 +1,3 @@
-import re
-
-strategy_map = {
-    "count": lambda o: int(o['lindex']) <= o['password'].count(o['letter']) <= int(o['rindex']),
-    "position": lambda o: (o.password[int(o['lindex'])-1] == o['letter']) != (o['password'][o['rindex']-1] == o['letter'])
-}
-
-# 2-4 r: prrmspx
-regex = re.compile(r"(?P<lindex>\d+)-(?P<rindex>\d+) (?P<letter>\w): (?P<password>\w+)")
-if __name__ == '__main__':
-    is_valid = strategy_map["count"]
-    with open('input.txt') as f:
-        print(sum(is_valid(regex.match(line).groupdict()) for line in f))
+import re;i=int;r=re.compile(r"(\w+)-(\w+) (\w): (\w+)")
+for s in (lambda o:i(o[0])<=o[3].count(o[2])<=i(o[1]),lambda o:(o[3][i(o[0])-1]==o[2])!=(o[3][i(o[1])-1]==o[2])):
+  with open('i') as f: print(sum(s(r.match(l).groups())for l in f))
--- 2020/day02/kev/solution_kl.py
+++ 2020/day02/kev/solution_kl.py
@@ -1,12 +1,5 @@
 import pathlib
 
-def count_chars(string, char_):
-    count = 0
-    for c in string:
-        if c == char_:
-            count += 1
-    return count
-
 
 if __name__ == '__main__':
     input_file = pathlib.Path('input.txt')
@@ -16,6 +9,6 @@ if __name__ == '__main__':
             rule, letter, password = l.split(" ")
             rule_min, rule_max = (int(i) for i in rule.split("-"))
             letter = letter.strip(':')
-            if rule_min <= count_chars(password, letter) <= rule_max:
+            if rule_min <= password.count(letter) <= rule_max:
                 count += 1
     print(count)
