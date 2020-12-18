ff00a955f5026454415402b524ee9e640d49a775
Yann Parizeau <yparizeau@innovmetric.com>
2020-12-18 12:47:49 -0500

    fix typo

--- 2020/day04/yp/solution.py
+++ 2020/day04/yp/solution.py
@@ -1,24 +1,24 @@
 import itertools
 
 
-def get_passwords():
-    pw = {}
+def get_passports():
+    pp = {}
     with open('input.txt') as f:
         for line in f:
             if not line.strip():
-                yield pw
-                pw = {}
+                yield pp
+                pp = {}
                 continue
-            pw.update(dict(kws.split(':') for kws in line.split()))
-    yield pw
+            pp.update(dict(kws.split(':') for kws in line.split()))
+    yield pp
 
 
-def filter_part_one(pws):
+def filter_part_one(pps):
     mandatory_keys = {'byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid'}
-    return [pw for pw in pws if not mandatory_keys - set(pw)]
+    return [pp for pp in pps if not mandatory_keys - set(pp)]
 
 
-def filter_part_two(pws):
+def filter_part_two(pps):
     valid_byr = lambda byr: len(byr) == 4 and 1920 <= int(byr) <= 2002
     valid_iyr = lambda iyr: len(iyr) == 4 and 2010 <= int(iyr) <= 2020
     valid_eyr = lambda eyr: len(eyr) == 4 and 2020 <= int(eyr) <= 2030
@@ -27,10 +27,10 @@ def filter_part_two(pws):
     valid_ecl = lambda ecl: ecl in {'amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'}
     valid_pid = lambda pid: len(pid) == 9 and not set(pid) - set(map(str, range(10)))
     return [
-        pw for pw in pws if valid_byr(pw['byr']) and valid_iyr(pw['iyr']) and valid_eyr(pw['eyr'])
-        and valid_hgt(pw['hgt']) and valid_hcl(pw['hcl']) and valid_ecl(pw['ecl']) and valid_pid(pw['pid'])]
+        pp for pp in pps if valid_byr(pp['byr']) and valid_iyr(pp['iyr']) and valid_eyr(pp['eyr'])
+        and valid_hgt(pp['hgt']) and valid_hcl(pp['hcl']) and valid_ecl(pp['ecl']) and valid_pid(pp['pid'])]
 
 
-part_one = filter_part_one(get_passwords())
+part_one = filter_part_one(get_passports())
 print('Part 1: ', len(part_one))
 print('Part 2: ', len(filter_part_two(part_one)))
